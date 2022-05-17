61ba87656db9fa218076d533087170e5014c8298
Boyan Boyanov <88735692+boyan-boyanov@users.noreply.github.com>
2022-05-17 23:18:16 +0300

    Delete CatShop directory

--- CatShop/addBreed.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-
-const router = Router();
-
-router.get('/cats/add-breed', (req, res) => {
-    res.sendFile(__dirname + '/resources/views/addBreed.html')
-});
-
-module.exports = router;
--- CatShop/test.js
+++
@@ -1,6 +0,0 @@
-const template = `
-<h1>Test Templating</h1>
-<p>Трейнинг</p>
-`
-
-module.exports = {template}
