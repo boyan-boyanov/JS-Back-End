2d1ede133fad7b8e0df992f6b0e9f86c28b20604
Boyan Boyanov <88735692+boyan-boyanov@users.noreply.github.com>
2022-05-28 18:18:10 +0300

    set Handlebars

--- Cubes/src/index.js
+++ Cubes/src/index.js
@@ -1,10 +1,19 @@
+// стартирай с npm start  - настройка в package.json
 const express = require('express')
-const hbs = require('express-handlebars')
+const handlebars = require('express-handlebars')
 
 const app = express()
 
+app.use('/static', express.static("public")) //на всеки рекуест към /static използвай express.static()
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+})) // използвай енджина с името hbs от handlebars , използвай и разширението hbs
+app.set('view engine', 'hbs') // сетни hbs разширение за всичи файлове
+app.set('views', './src/views') // търси вютата на този път
+
+
 app.get('/', (req, res) => {
-    res.send("test")
+    res.render("index")
 })
 
 
