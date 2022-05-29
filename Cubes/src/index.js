930111a13785227112a2d898e38f46f1fbc3fcff
Boyan Boyanov <88735692+boyan-boyanov@users.noreply.github.com>
2022-05-29 13:13:40 +0300

    set navigation

--- Cubes/src/index.js
+++ Cubes/src/index.js
@@ -16,5 +16,20 @@ app.get('/', (req, res) => {
     res.render("index")
 })
 
+app.get('/create', (req,res)=>{
+    res.render('create')
+})
+
+app.get('/about', (req,res)=>{
+    res.render('about')
+})
+
+app.get('/details/:id', (req,res)=>{
+    res.render('details')
+})
+
+app.get('*', (req,res)=>{
+    res.send('404')
+})
 
 app.listen(5000)
