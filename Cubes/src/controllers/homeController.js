d1c2ced41ce2eb31f8348c7f207b57f1255fd110
Boyan Boyanov <88735692+boyan-boyanov@users.noreply.github.com>
2022-05-29 18:08:18 +0300

    dynamic render cubes

--- Cubes/src/controllers/homeController.js
+++ Cubes/src/controllers/homeController.js
@@ -1,6 +1,7 @@
+const cubes = require('../db.json') //Взимаме данните от файла
 
 exports.index = (req, res) => {
-    res.render("index")
+    res.render("index", {cubes}) // подаваме данните от cubes за да се рендерират
 }
 
 exports.about = (req,res) => {
