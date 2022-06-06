099f59bff3877b6a50994be3d49d7b049b64b5cc
Boyan Boyanov <88735692+boyan-boyanov@users.noreply.github.com>
2022-06-06 22:04:27 +0300

    set mongoDB

--- Movies/index.js
+++ Movies/index.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const hbs = require("express-handlebars");
+const { MongoClient } = require('mongodb')
 
 const app = express();
+const url = 'mongodb://localhost:27017'
+const client = new MongoClient(url)
+
+client.connect()
+.then(()=>{
+    console.log('DB connecting done');
+})
+
+const db = client.db('movieDB')
+const movieCollection = db.collection('movies')
 
 app.engine('hbs', hbs.engine({
     extname: 'hbs'
@@ -13,4 +24,4 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.listen(5000, ()=> console.log('server is listening on port 5000.....'))
+app.listen(5000, () => console.log('server is listening on port 5000.....'))
