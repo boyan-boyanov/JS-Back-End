07f368be1815591f18e1deabde1abd580f63435a
Boyan Boyanov <88735692+boyan-boyanov@users.noreply.github.com>
2022-06-07 23:10:02 +0300

    add movie to mongoDB

--- Movies/index.js
+++ Movies/index.js
@@ -1,33 +1,31 @@
 const express = require("express");
 const hbs = require("express-handlebars");
-const { MongoClient } = require('mongodb')
+const mongoose = require('mongoose')
+
+const movieControler = require('./controllers/movieController')
 
 const app = express();
-const url = 'mongodb://localhost:27017'
-const client = new MongoClient(url)
+const url = 'mongodb://localhost:27017/movieDB'
+mongoose.connect(url)
+.then(()=> {
+    console.log('db connected');
+})
+.catch((err)=>{
+    console.log('db error: ', err);
+})
 
-client.connect()
-    .then(() => {
-        console.log('DB connecting done');
-    })
-
-const db = client.db('movieDB')
-const moviesCollection = db.collection('movies')
-
+app.use(express.urlencoded({extended: false}))
 app.engine('hbs', hbs.engine({
     extname: 'hbs'
 }));
 
 app.set('view engine', 'hbs');
 
-app.get('/', (req, res) => {
+app.get('/', (req,res)=> {
     res.render('home')
 })
 
-app.get('/movies', async (req, res) => {
-    let movies = await moviesCollection.find().toArray();
+app.use('/movies', movieControler)
 
-    res.render('movies', { movies })
-})
 
 app.listen(5000, () => console.log('server is listening on port 5000.....'))
