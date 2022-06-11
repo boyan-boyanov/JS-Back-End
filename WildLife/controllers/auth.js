4bf34f7e021153b8ea520bb6b2ab468a0a8d03f8
Boyan Boyanov <88735692+boyan-boyanov@users.noreply.github.com>
2022-06-11 21:19:16 +0300

    add Guards

--- WildLife/controllers/auth.js
+++ WildLife/controllers/auth.js
@@ -1,15 +1,17 @@
+const { route } = require('express/lib/application');
+const { isUser, isGuest } = require('../middleware/gards');
 const { register, login } = require('../services/userServices');
 const mapErrors = require('../util/mappers');
 
 const router = require('express').Router();
 
 
-router.get('/register', (req, res) => {
+router.get('/register', isGuest(), (req, res) => {
     res.render('register');
 })
 
 // TODO check form action, method, field names
-router.post('/register', async (req, res) => {
+router.post('/register', isGuest(), async (req, res) => {
 
     try {
         if (req.body.password != req.body.repass) {
@@ -28,12 +30,12 @@ router.post('/register', async (req, res) => {
 
 })
 
-router.get('/login', (req, res) => {
+router.get('/login', isGuest(), (req, res) => {
     res.render('login')
 })
 
 //TODO check form action , method, field names
-router.post('/login', async (req, res) => {
+router.post('/login', isGuest(), async (req, res) => {
     try {
         const user = await login(req.body.username, req.body.password);
         req.session.user = user;
@@ -44,6 +46,11 @@ router.post('/login', async (req, res) => {
         const errors = mapErrors(err);
         res.render('login', { data: { username: req.body.username }, errors })
     }
+})
+
+router.get('/logout', isUser(), (req, res) => {
+    delete req.session.user;
+    res.redirect('/')
 })
 
 module.exports = router;
